394e64965dc0b71bfc05052cc5cd6e24c2fa465a
Leilson <leilsonf@gmail.com>
2022-05-01 23:05:46 -0300

    acrescentei join categorias_pai

--- application/models/Produtos_model.php
+++ application/models/Produtos_model.php
@@ -4,7 +4,7 @@
 	class Produtos_model extends CI_Model {
 		
 		public function get_all() {
-			$this->db->select([
+			$this->db->select([				
 				'produtos.produto_id',
 				'produtos.produto_codigo',
 				'produtos.produto_nome',
@@ -12,12 +12,17 @@
 				'produtos.produto_ativo',
 				'categorias.categoria_id',
 				'categorias.categoria_nome',
+				'categorias_pai.categoria_pai_id',
+				'categorias_pai.categoria_pai_nome',
 				'marcas.marca_nome',			
-			]);
+			]);			
 			
 			$this->db->join('categorias', 'categorias.categoria_id = produtos.produto_categoria_id', 'LEFT');
-			$this->db->join('marcas', 'marcas.marca_id = produtos.produto_marca_id', 'LEFT');
-			
+			$this->db->join('marcas', 'marcas.marca_id = produtos.produto_marca_id', 'LEFT');       
+			$this->db->join('categorias_pai', 'categorias_pai.categoria_pai_id = categorias.categoria_pai_id', 'LEFT');			
+		
 			return $this->db->get('produtos')->result();
 		}		
 	}	
+	
+	
