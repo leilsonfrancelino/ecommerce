180f5a4d83398c5b6cd7909b812eca7aae55e6a9
leilsonfrancelino <31639177+leilsonfrancelino@users.noreply.github.com>
2022-05-05 13:17:36 -0300

    Mudança de local do js para ckeditor e selects dinamicos.
    Mudança de local do js para ckeditor e selects dinamicos.

--- application/views/restrita/layout/footer.php
+++ application/views/restrita/layout/footer.php
@@ -1,4 +1,4 @@
-<?php if($this->router->fetch_class() != 'login') : ?>	
+<<?php if($this->router->fetch_class() != 'login') : ?>	
 	<footer class="main-footer">
         <div class="footer-left">
           <a href="templateshub.net">Templateshub</a></a>
@@ -41,8 +41,44 @@
 		});
   </script>
   
-</body>
+  <!-- JS CKEditor -->
+  <script type="text/javascript" src="<?php echo base_url().'public/ckeditor/ckeditor.js'?>"></script>
+  <!-- adicionando editor no cadastro e edição de produtos -->	
+  <script>CKEDITOR.replace( 'produto_descricao' );</script>
+  
+	<!-- JS para os selects categoria e subcategoria -->	
+	<script type="text/javascript" src="<?php echo base_url().'public/assets/js/jquery-3.3.1.js'?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'public/assets/js/bootstrap.js'?>"></script>
+	<!-- JS para preencher select subcategoria de acordo com a escolha da categoria em cadastro de produto -->
+	<script type="text/javascript">
+		$(document).ready(function(){
 
+			$('#produto_categoria_pai_id').change(function(){ 
+				var id=$(this).val();
+				
+				$.ajax({
+					url : "<?php echo site_url('restrita/produtos/get_sub_category');?>",
+					method : "POST",
+					data : {id: id},
+					async : true,
+					dataType : 'json',
+					success: function(data){
+						console.log(data);
+						var html = '';
+						var i;
+						for(i=0; i<data.length; i++){
+							html += '<option value='+data[i].categoria_id+'>'+data[i].categoria_nome+'</option>';
+						}
+						$('#sub_category').html(html);
+					}
+				});
+				return false;
+			}); 
+			
+			});
+	</script>
+  
+</body>
 
 <!-- blank.html  21 Nov 2019 03:54:41 GMT -->
 </html>
