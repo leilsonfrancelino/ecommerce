4848747250a43473cf304695a641c01abf4b3d28
leilsonfrancelino <31639177+leilsonfrancelino@users.noreply.github.com>
2022-05-09 16:22:45 -0300

    acrescentando caracteres especiais.
    acrescentando caracteres especiais.

--- application/helpers/recursos_helper.php
+++ application/helpers/recursos_helper.php
@@ -49,8 +49,8 @@ function url_amigavel($string = NULL) {
 }
 
 function remove_acentos($string = NULL) {
-    $procurar = array('À', 'Á', 'Ã', 'Â', 'É', 'Ê', 'Í', 'Ó', 'Õ', 'Ô', 'Ú', 'Ü', 'Ç', 'à', 'á', 'ã', 'â', 'é', 'ê', 'í', 'ó', 'õ', 'ô', 'ú', 'ü', 'ç');
-    $substituir = array('a', 'a', 'a', 'a', 'e', 'r', 'i', 'o', 'o', 'o', 'u', 'u', 'c', 'a', 'a', 'a', 'a', 'e', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'c');
+    $procurar = array('À', 'Á', 'Ã', 'Â', 'É', 'Ê', 'Í', 'Ó', 'Õ', 'Ô', 'Ú', 'Ü', 'Ç', 'à', 'á', 'ã', 'â', 'é', 'ê', 'í', 'ó', 'õ', 'ô', 'ú', 'ü', 'ç', 'ª', 'º');
+    $substituir = array('a', 'a', 'a', 'a', 'e', 'r', 'i', 'o', 'o', 'o', 'u', 'u', 'c', 'a', 'a', 'a', 'a', 'e', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'c', '', '');
     return str_replace($procurar, $substituir, $string);
 }
 
