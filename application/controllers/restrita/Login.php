279b6947efcd081e915a8d43bed1da16e57286b3
leilsonfrancelino <31639177+leilsonfrancelino@users.noreply.github.com>
2022-05-16 14:11:55 -0300

    proibindo acesso de cliente no admin
    proibindo acesso de cliente no admin

--- application/controllers/restrita/Login.php
+++ application/controllers/restrita/Login.php
@@ -1,36 +1,44 @@
 <?php
 defined('BASEPATH') OR exit('Ação não permitida');
+	<?php
+	defined('BASEPATH') OR exit('Ação não permitida');
 
-class Login extends CI_Controller {	
-	
-	public function index() {
+	class Login extends CI_Controller {	
 		
-		$data = array(
-			'titulo' => 'Login na área restrita',
-		);
+		public function index() {
+			
+			$data = array(
+				'titulo' => 'Login na área restrita',
+			);
+			
+			$this->load->view('restrita/layout/header', $data);
+			$this->load->view('restrita/login/index');
+			$this->load->view('restrita/layout/footer');
+		}
 		
-		$this->load->view('restrita/layout/header', $data);
-		$this->load->view('restrita/login/index');
-		$this->load->view('restrita/layout/footer');
-	}
-	
-	public function auth() {
-		$identity = $this->input->post('email');
-		$password = $this->input->post('password');
-		$remember = $this->input->post('remember' ? TRUE : FALSE);
+		public function auth() {
+			$identity = $this->input->post('email');
+			$password = $this->input->post('password');
+			$remember = $this->input->post('remember' ? TRUE : FALSE);
+			
+			
+			if($this->ion_auth->login($identity, $password,$remember)) {
+				 if($this->ion_auth->is_admin()){					
+					 $this->session->set_flashdata('sucesso', 'Seja bem vindo(a)!');
+					 redirect('restrita');
+				 }else{
+					 $this->session->set_flashdata('erro', 'Você não tem permisão de administrador!');
+					 redirect('restrita/login');
+				 }
+			}else{
+				$this->session->set_flashdata('erro', 'Não foi possível efetuar o login!');
+				redirect('restrita/login');
+			}
+		}
 		
-		
-		if($this->ion_auth->login($identity, $password,$remember)) {
-			$this->session->set_flashdata('sucesso', 'Seja bem vindo(a)!');
-			redirect('restrita');
-		}else{
-			$this->session->set_flashdata('erro', 'Não foi possível efetuar o login!');
+		public function logout() {
+			$this->ion_auth->logout();
 			redirect('restrita/login');
 		}
 	}
-	
-	public function logout() {
-		$this->ion_auth->logout();
-		redirect('restrita/login');
-	}
-}
+
