ab73ddad1993e04c27ebec6c9dbe927e50096c21
leilsonfrancelino <31639177+leilsonfrancelino@users.noreply.github.com>
2022-05-19 15:05:01 -0300

    acertos no rodape
    acertos no rodape

--- application/views/web/layout/footer.php
+++ application/views/web/layout/footer.php
@@ -13,8 +13,8 @@
                                             <img src="<?php echo base_url('public/web/images/shipping-icon/1.png')?>" alt="Shipping Icon">
                                         </div>
                                         <div class="shipping-text">
-                                            <h2>Free Delivery</h2>
-                                            <p>And free returns. See checkout for delivery dates.</p>
+                                            <h2>Entrega Grátis</h2>
+                                            <p>Consulte as regras.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -26,8 +26,8 @@
                                             <img src="<?php echo base_url('public/web/images/shipping-icon/2.pn')?>g" alt="Shipping Icon">
                                         </div>
                                         <div class="shipping-text">
-                                            <h2>Safe Payment</h2>
-                                            <p>Pay with the world's most popular and secure payment methods.</p>
+                                            <h2>Pagamento Seguro</h2>
+                                            <p>Pague com as formas de pagamento mais populares e seguros.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -39,8 +39,8 @@
                                             <img src="<?php echo base_url('public/web/images/shipping-icon/3.png')?>" alt="Shipping Icon">
                                         </div>
                                         <div class="shipping-text">
-                                            <h2>Shop with Confidence</h2>
-                                            <p>Our Buyer Protection covers your purchasefrom click to delivery.</p>
+                                            <h2>Compra Segura</h2>
+                                            <p>Proteção ao comprador, cobre sua compra desde o clique até a entrega.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -52,8 +52,8 @@
                                             <img src="<?php echo base_url('public/web/images/shipping-icon/4.png')?>" alt="Shipping Icon">
                                         </div>
                                         <div class="shipping-text">
-                                            <h2>24/7 Help Center</h2>
-                                            <p>Have a question? Call a Specialist or chat online.</p>
+                                            <h2>Central de Ajuda</h2>
+                                            <p>Ligue para nossos atendentes ou converse online.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -97,12 +97,14 @@
                                 <!-- Begin Footer Block Area -->
                                 <div class="col-lg-2 col-md-3 col-sm-6">
                                     <div class="footer-block">
-                                        <h3 class="footer-block-title">Product</h3>
+                                        <h3 class="footer-block-title">Departamentos</h3>
                                         <ul>
-                                            <li><a href="#">Prices drop</a></li>
-                                            <li><a href="#">New products</a></li>
-                                            <li><a href="#">Best sales</a></li>
-                                            <li><a href="#">Contact us</a></li>
+                                            <li><a href="<?php echo base_url('master/eletronicos'); ?>">Eletrônicos</a></li>
+                                            <li><a href="<?php echo base_url('master/games'); ?>">Games</a></li>
+                                            <li><a href="<?php echo base_url('master/hardware'); ?>">Hardware</a></li>
+                                            <li><a href="<?php echo base_url('master/informatica'); ?>">Informática</a></li>
+											<li><a href="<?php echo base_url('master/perifericos'); ?>">Periféricos</a></li>
+                                            <li><a href="<?php echo base_url('master/telefonia'); ?>">Telefonia</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -110,12 +112,12 @@
                                 <!-- Begin Footer Block Area -->
                                 <div class="col-lg-2 col-md-3 col-sm-6">
                                     <div class="footer-block">
-                                        <h3 class="footer-block-title">Our company</h3>
+                                        <h3 class="footer-block-title">Nossa Empresa</h3>
                                         <ul>
-                                            <li><a href="#">Delivery</a></li>
-                                            <li><a href="#">Legal Notice</a></li>
-                                            <li><a href="#">About us</a></li>
-                                            <li><a href="#">Contact us</a></li>
+                                            <li><a href="#">Entrega</a></li>
+                                            <li><a href="#">Política e Privacidade</a></li>
+                                            <li><a href="#">Sobre</a></li>
+                                            <li><a href="#">Contato</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -123,7 +125,7 @@
                                 <!-- Begin Footer Block Area -->
                                 <div class="col-lg-4">
                                     <div class="footer-block">
-                                        <h3 class="footer-block-title">Follow Us</h3>
+                                        <h3 class="footer-block-title">Siga-nos</h3>
                                         <ul class="social-link">
                                             <li class="twitter">
                                                 <a href="https://twitter.com/" data-toggle="tooltip" target="_blank" title="Twitter">
@@ -159,12 +161,12 @@
                                     </div>
                                     <!-- Begin Footer Newsletter Area -->
                                     <div class="footer-newsletter">
-                                        <h4>Sign up to newsletter</h4>
+                                        <h4>Inscreva-se em nossa Newsletter</h4>
                                         <form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="footer-subscribe-form validate" target="_blank" novalidate>
                                            <div id="mc_embed_signup_scroll">
                                               <div id="mc-form" class="mc-form subscribe-form form-group" >
-                                                <input id="mc-email" type="email" autocomplete="off" placeholder="Enter your email" />
-                                                <button  class="btn" id="mc-submit">Subscribe</button>
+                                                <input id="mc-email" type="email" autocomplete="off" placeholder="Digite seu email" />
+                                                <button  class="btn" id="mc-submit">Inscrever-se</button>
                                               </div>
                                            </div>
                                         </form>
@@ -225,119 +227,7 @@
                 </div>
                 <!-- Footer Static Bottom Area End Here -->
             </div>
-            <!-- Footer Area End Here -->
-            <!-- Begin Quick View | Modal Area -->
-            <div class="modal fade modal-wrapper" id="exampleModalCenter" >
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            <div class="modal-inner-area row">
-                                <div class="col-lg-5 col-md-6 col-sm-6">
-                                   <!-- Product Details Left -->
-                                    <div class="product-details-left">
-                                        <div class="product-details-images slider-navigation-1">
-                                            <div class="lg-image">
-                                                <img src="<?php echo base_url('public/web/images/product/large-size/1.jpg')?>" alt="product image">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="<?php echo base_url('public/web/images/product/large-size/2.jpg')?>" alt="product image">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="<?php echo base_url('public/web/images/product/large-size/3.jpg')?>" alt="product image">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="<?php echo base_url('public/web/images/product/large-size/4.jpg')?>" alt="product image">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="<?php echo base_url('public/web/images/product/large-size/5.jpg')?>" alt="product image">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="<?php echo base_url('public/web/images/product/large-size/6.jpg')?>" alt="product image">
-                                            </div>
-                                        </div>
-                                        <div class="product-details-thumbs slider-thumbs-1">                                        
-                                            <div class="sm-image"><img src="<?php echo base_url('public/web/images/product/small-size/1.jpg')?>" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="<?php echo base_url('public/web/images/product/small-size/2.jpg')?>" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="<?php echo base_url('public/web/images/product/small-size/3.jpg')?>" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="<?php echo base_url('public/web/images/product/small-size/4.jpg')?>" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="<?php echo base_url('public/web/images/product/small-size/5.jpg')?>" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="<?php echo base_url('public/web/images/product/small-size/6.jpg')?>" alt="product image thumb"></div>
-                                        </div>
-                                    </div>
-                                    <!--// Product Details Left -->
-                                </div>
-
-                                <div class="col-lg-7 col-md-6 col-sm-6">
-                                    <div class="product-details-view-content pt-60">
-                                        <div class="product-info">
-                                            <h2>Today is a good day Framed poster</h2>
-                                            <span class="product-details-ref">Reference: demo_15</span>
-                                            <div class="rating-box pt-20">
-                                                <ul class="rating rating-with-review-item">
-                                                    <li><i class="fa fa-star-o"></i></li>
-                                                    <li><i class="fa fa-star-o"></i></li>
-                                                    <li><i class="fa fa-star-o"></i></li>
-                                                    <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                    <li class="no-star"><i class="fa fa-star-o"></i></li>
-                                                    <li class="review-item"><a href="#">Read Review</a></li>
-                                                    <li class="review-item"><a href="#">Write Review</a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="price-box pt-20">
-                                                <span class="new-price new-price-2">$57.98</span>
-                                            </div>
-                                            <div class="product-desc">
-                                                <p>
-                                                    <span>100% cotton double printed dress. Black and white striped top and orange high waisted skater skirt bottom. Lorem ipsum dolor sit amet, consectetur adipisicing elit. quibusdam corporis, earum facilis et nostrum dolorum accusamus similique eveniet quia pariatur.
-                                                    </span>
-                                                </p>
-                                            </div>
-                                            <div class="product-variants">
-                                                <div class="produt-variants-size">
-                                                    <label>Dimension</label>
-                                                    <select class="nice-select">
-                                                        <option value="1" title="S" selected="selected">40x60cm</option>
-                                                        <option value="2" title="M">60x90cm</option>
-                                                        <option value="3" title="L">80x120cm</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="single-add-to-cart">
-                                                <form action="#" class="cart-quantity">
-                                                    <div class="quantity">
-                                                        <label>Quantity</label>
-                                                        <div class="cart-plus-minus">
-                                                            <input class="cart-plus-minus-box" value="1" type="text">
-                                                            <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
-                                                            <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div>
-                                                        </div>
-                                                    </div>
-                                                    <button class="add-to-cart" type="submit">Add to cart</button>
-                                                </form>
-                                            </div>
-                                            <div class="product-additional-info pt-25">
-                                                <a class="wishlist-btn" href="wishlist.html"><i class="fa fa-heart-o"></i>Add to wishlist</a>
-                                                <div class="product-social-sharing pt-25">
-                                                    <ul>
-                                                        <li class="facebook"><a href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
-                                                        <li class="twitter"><a href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
-                                                        <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i>Google +</a></li>
-                                                        <li class="instagram"><a href="#"><i class="fa fa-instagram"></i>Instagram</a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>   
-            <!-- Quick View | Modal Area End Here -->
+            <!-- Footer Area End Here -->           
         </div>
         <!-- Body Wrapper End Here -->
         <!-- jQuery-V1.12.4 -->
