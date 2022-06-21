721011359786d9ef14ad04590c9ac66da13f33cf
leilsonfrancelino <31639177+leilsonfrancelino@users.noreply.github.com>
2022-06-21 10:50:44 -0300

    Libras
    Libras

--- application/views/web/layout/header.php
+++ application/views/web/layout/header.php
@@ -53,6 +53,12 @@
 		
     </head>
     <body>
+    	<div vw class="enabled">
+	    <div vw-access-button class="active"></div>
+	    <div vw-plugin-wrapper>
+	      <div class="vw-plugin-top-wrapper"></div>
+	    </div>
+  	</div>
     <!--[if lt IE 8]>
 		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 	<![endif]-->
